9c0b2e15f1d720d013e5c58c8ce7b5662fa74da1
sonde756 <edsonrafael756@gmail.com>
2023-06-11 23:48:36 -0300

    many-to-many association with JoinTable

--- src/main/java/com/projectspring/course/config/TestConfig.java
+++ src/main/java/com/projectspring/course/config/TestConfig.java
@@ -58,8 +58,14 @@ public class TestConfig implements CommandLineRunner {
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
 		
-		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 
 	}
 }
