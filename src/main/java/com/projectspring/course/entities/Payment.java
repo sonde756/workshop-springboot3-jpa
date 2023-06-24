0d741f1655a6cfbc1f3a5a4f1853fb83720a8bdf
sonde756 <edsonrafael756@gmail.com>
2023-06-24 20:52:28 -0300

    Payment, one-to-one association

---
+++ src/main/java/com/projectspring/course/entities/Payment.java
@@ -0,0 +1,80 @@
+package com.projectspring.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Instant momoment;
+	
+	@OneToOne
+	@MapsId
+	private Order order;
+
+	public Payment() {}
+
+	public Payment(Long id, Instant momoment, Order order) {
+		super();
+		this.id = id;
+		this.momoment = momoment;
+		this.order = order;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMomoment() {
+		return momoment;
+	}
+
+	public void setMomoment(Instant momoment) {
+		this.momoment = momoment;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
